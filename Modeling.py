667f13b6408b545f34b94a2b1b2bd5e8e0cf4525
Christopher Lach <32966115+clach1225@users.noreply.github.com>
2018-12-04 08:11:50 -0700

    Add files via upload

---
+++ Modeling.py
@@ -0,0 +1,138 @@
+import random
+import pandas as pd
+import os
+
+from sklearn.model_selection import train_test_split
+
+
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import numpy as np
+
+#from PIL import Image, ImageFilter
+
+import keras.backend as KerasBackend
+
+from skimage.transform import resize
+
+import time
+
+
+from keras.datasets import mnist
+from keras.models import Model
+from keras.layers import Input, Concatenate, Dense, Dropout, Flatten, Activation
+from keras.layers.convolutional import Conv2D, MaxPooling2D
+from keras.layers import Conv2DTranspose
+from keras.layers.normalization import BatchNormalization
+from keras.utils import to_categorical
+from keras import backend as K
+from keras.optimizers import Adam
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.layers import UpSampling2D
+from keras.layers import ZeroPadding2D
+from keras.preprocessing.image import ImageDataGenerator
+import keras
+# import cv2
+
+import tensorflow as tf
+import keras.utils as np_utils
+
+
+import tqdm
+
+from keras.preprocessing import image
+from keras.layers import Lambda
+from keras.layers import Concatenate
+
+from keras.callbacks import EarlyStopping
+from tqdm import tqdm_notebook as tqdm_NB
+import os
+import shutil
+
+
+
+import skimage.io as io
+import skimage.transform as trans
+
+
+
+from keras.models import *
+from keras.layers import *
+from keras.optimizers import *
+from keras.callbacks import ModelCheckpoint, LearningRateScheduler
+from keras import backend as keras
+
+import re
+
+from IPython.core.display import display, HTML
+
+
+import sys
+import warnings
+
+if not sys.warnoptions:
+    warnings.simplefilter("ignore")
+
+from mean_iou import mean_iou
+
+def UNET_MODEL(X_train_w_aug,y_train_w_aug, X_test_out_of_train, y_test_out_of_train):
+    # Build U-Net model
+    im_height = 128
+    im_width = 128
+    im_chan = 1
+
+    inputs = Input((im_height, im_width, im_chan))
+    s = Lambda(lambda x: x / 255) (inputs)
+
+    c1 = Conv2D(8, (3, 3), activation='relu', padding='same') (s)
+    c1 = Conv2D(8, (3, 3), activation='relu', padding='same') (c1)
+    p1 = MaxPooling2D((2, 2)) (c1)
+
+    c2 = Conv2D(16, (3, 3), activation='relu', padding='same') (p1)
+    c2 = Conv2D(16, (3, 3), activation='relu', padding='same') (c2)
+    p2 = MaxPooling2D((2, 2)) (c2)
+
+    c3 = Conv2D(32, (3, 3), activation='relu', padding='same') (p2)
+    c3 = Conv2D(32, (3, 3), activation='relu', padding='same') (c3)
+    p3 = MaxPooling2D((2, 2)) (c3)
+
+    c4 = Conv2D(64, (3, 3), activation='relu', padding='same') (p3)
+    c4 = Conv2D(64, (3, 3), activation='relu', padding='same') (c4)
+    p4 = MaxPooling2D(pool_size=(2, 2)) (c4)
+
+    c5 = Conv2D(128, (3, 3), activation='relu', padding='same') (p4)
+    c5 = Conv2D(128, (3, 3), activation='relu', padding='same') (c5)
+
+    u6 = Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same') (c5)
+    u6 = concatenate([u6, c4])
+    c6 = Conv2D(64, (3, 3), activation='relu', padding='same') (u6)
+    c6 = Conv2D(64, (3, 3), activation='relu', padding='same') (c6)
+
+    u7 = Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same') (c6)
+    u7 = concatenate([u7, c3])
+    c7 = Conv2D(32, (3, 3), activation='relu', padding='same') (u7)
+    c7 = Conv2D(32, (3, 3), activation='relu', padding='same') (c7)
+
+    u8 = Conv2DTranspose(16, (2, 2), strides=(2, 2), padding='same') (c7)
+    u8 = concatenate([u8, c2])
+    c8 = Conv2D(16, (3, 3), activation='relu', padding='same') (u8)
+    c8 = Conv2D(16, (3, 3), activation='relu', padding='same') (c8)
+
+    u9 = Conv2DTranspose(8, (2, 2), strides=(2, 2), padding='same') (c8)
+    u9 = concatenate([u9, c1], axis=3)
+    c9 = Conv2D(8, (3, 3), activation='relu', padding='same') (u9)
+    c9 = Conv2D(8, (3, 3), activation='relu', padding='same') (c9)
+
+    outputs = Conv2D(1, (1, 1), activation='sigmoid') (c9)
+
+    model = Model(inputs=[inputs], outputs=[outputs])
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=[mean_iou])
+    model.summary()
+
+    earlystopper = EarlyStopping(patience=5, verbose=2)
+    checkpointer = ModelCheckpoint('model-tgs-salt-1.h5', verbose=2, save_best_only=True)
+    results = model.fit(X_train_w_aug, y_train_w_aug, validation_data=(X_test_out_of_train, y_test_out_of_train),
+                        batch_size=8, epochs=30, verbose=2,
+                        callbacks=[earlystopper, checkpointer])
